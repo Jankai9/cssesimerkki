40c84761a7ee7eed6a8b8fc3025a828a5f937eba
Janne Kaipala <janne.kaipala@hiq.fi>
2024-01-12 16:55:31 +0200

    css example

---
+++ src/Css.js
@@ -0,0 +1,21 @@
+import styles from './Css.module.scss';
+
+export default function Css() {
+  console.log(styles);
+  return (
+    <div class={styles.esimerkki}>
+      <div class={styles.container}>
+        <div id='box1' class={styles.box1}>
+          1
+        </div>
+        <div id='box2' class={styles.box2}>
+          2
+        </div>
+        <div id='box3' class={styles.box3}>
+          3
+        </div>
+      </div>
+    <a>punainen väri: div.esimerkki > a</a>
+    </div>
+  );
+}
